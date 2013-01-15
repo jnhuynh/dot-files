0c59d5cdee1a78f6dd61c41b7d291b9e3549c62f
John Huynh <j.n.huynh@me.com>
2013-01-15 13:58:25 -0500

    Added ln options so it doesn't follow sym links.

--- install.sh
+++ install.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 echo "--- Setting up Bash ---"
 echo "Creating a symbolic link for .bash_profile"
-ln -s $HOME/.DotFiles/Bash/bash_profile.bash $HOME/.bash_profile
+ln -Ffhs $HOME/.DotFiles/Bash/bash_profile.bash $HOME/.bash_profile
 echo
 
 echo "--- Setting up Vim ---"
 echo "Creating a symbolic link for .vimrc"
-ln -s $HOME/.DotFiles/Vim/vimrc.vim $HOME/.vimrc
+ln -Ffhs $HOME/.DotFiles/Vim/vimrc.vim $HOME/.vimrc
 echo "Creating a symbolic link for .vimrc"
-ln -s $HOME/.DotFiles/Vim/gvimrc.vim $HOME/.gvimrc
+ln -Ffhs $HOME/.DotFiles/Vim/gvimrc.vim $HOME/.gvimrc
 echo "Creating a symbolic link for .vim"
-ln -s $HOME/.DotFiles/Vim/vim $HOME/.vim
+ln -Ffhs $HOME/.DotFiles/Vim/vim $HOME/.vim
 echo
 
 echo "--- Setting up Git ---"
 echo "Creating a symbolic link for .gitconfig"
-ln -s $HOME/.DotFiles/Git/gitconfig.git $HOME/.gitconfig
+ln -Ffhs $HOME/.DotFiles/Git/gitconfig.git $HOME/.gitconfig
 echo "Creating a symbolic link for .gitignore_global"
-ln -s $HOME/.DotFiles/Git/gitignore_global.git $HOME/.gitignore_global
+ln -Ffhs $HOME/.DotFiles/Git/gitignore_global.git $HOME/.gitignore_global
+echo
+
+echo "--- Creating and checking out local branch (development) ---"
+pushd $HOME/.Dotfiles
+git checkout master
+git branch development
+git checkout development
 echo
 
 echo "--- Done ---"
