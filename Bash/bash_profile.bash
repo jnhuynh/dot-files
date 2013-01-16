6f7fb37dbc2fbe5e8b3d01fa9028ea036ffb4af7
John Huynh <j.n.huynh@me.com>
2013-01-16 12:45:21 -0500

    Added link to where I found silent command.

--- Bash/bash_profile.bash
+++ Bash/bash_profile.bash
@@ -30,6 +30,7 @@ mesg n
 
 # http://jondavidjohn.com/blog/2012/12/how-to-accomplish-a-responsive-bash-prompt
 parse_git_branch() {
+    # http://stackoverflow.com/a/617184 <= Silent commands.
     if git rev-parse > /dev/null 2>/dev/null; then
         branch=`git branch | grep -e ^* | awk '{ print $2 }'`
     fi
