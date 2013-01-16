a6f7d02df46eb13099bfb6e33f486eea07a53d08
John Huynh <j.n.huynh@me.com>
2013-01-16 12:39:43 -0500

    Git branch now displays in sub-directories.

--- Bash/bash_profile.bash
+++ Bash/bash_profile.bash
@@ -30,7 +30,7 @@ mesg n
 
 # http://jondavidjohn.com/blog/2012/12/how-to-accomplish-a-responsive-bash-prompt
 parse_git_branch() {
-    if [ -d ".git" ]; then
+    if git rev-parse > /dev/null 2>/dev/null; then
         branch=`git branch | grep -e ^* | awk '{ print $2 }'`
     fi
 
