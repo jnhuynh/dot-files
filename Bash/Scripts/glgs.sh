520c5d728c5111a4c0e9afc55231055068d8c588
John Huynh <j.n.huynh@me.com>
2013-01-17 10:46:09 -0500

    Updated glgs to display information better.

--- Bash/Scripts/glgs.sh
+++ Bash/Scripts/glgs.sh
@@ -2,15 +2,21 @@
 
 set -e # If any commands in script fails the whole script will abort.
 
+echo
+
 # awk is used here to print the entire line to stdout since
 # git log outputs to Vim, which is it's default page.
-git log --reverse --pretty=format:"%H - %an, %ar : %s" -5 |
+git log --reverse --pretty=format:"%an, %ar : %s%n%H" -5 |
 awk '{ print }' |
 while read log; do
   echo REV:
+  echo $log # First line is the commit message and info.
+  read log # Second line is the commit hash.
   echo $log
   echo $log |
   awk '{ print $1 }' |
   xargs git show --pretty="format:" --name-only
+  echo
+  echo -------------------------------------------------
   echo
 done
