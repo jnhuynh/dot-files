c628a556e65192a371fd2bd751c97f02560a6b71
John Huynh <j.n.huynh@me.com>
2013-05-12 19:59:34 -0400

    Moved functions to utils.bash

--- Bash/bash_profile.bash
+++ Bash/bash_profile.bash
@@ -36,23 +36,6 @@ set -o noclobber
 # Prevents message spam
 mesg n
 
-# http://jondavidjohn.com/blog/2012/12/how-to-accomplish-a-responsive-bash-prompt
-parse_git_branch() {
-    # http://stackoverflow.com/a/617184 <= Silent commands.
-    if git rev-parse > /dev/null 2>/dev/null; then
-        branch=`git branch | grep -e ^* | awk '{ print $2 }'`
-    fi
-
-    if [[ $branch != "" ]]; then
-        echo " ($branch)"
-    fi
-}
-
-prompt() {
-    prompt="${BLUE}\h${NORMAL} |  \w${YELLOW}$(parse_git_branch)${NORMAL}\n$ "
-    PS1=$prompt
-}
-
 # https://coderwall.com/p/oqtj8w
 bind '"\e[A": history-search-backward' #up-arrow through history
 bind '"\e[B": history-search-forward' #down-arrow through history
--- Bash/utils.bash
+++ Bash/utils.bash
@@ -1,3 +1,20 @@
+# http://jondavidjohn.com/blog/2012/12/how-to-accomplish-a-responsive-bash-prompt
+parse_git_branch() {
+    # http://stackoverflow.com/a/617184 <= Silent commands.
+    if git rev-parse > /dev/null 2>/dev/null; then
+        branch=`git branch | grep -e ^* | awk '{ print $2 }'`
+    fi
+
+    if [[ $branch != "" ]]; then
+        echo " ($branch)"
+    fi
+}
+
+prompt() {
+    prompt="${BLUE}\h${NORMAL} |  \w${YELLOW}$(parse_git_branch)${NORMAL}\n$ "
+    PS1=$prompt
+}
+
 # set_rails_project
 # Usage: set_rails_project <project_path>
 function set_rails_project {
