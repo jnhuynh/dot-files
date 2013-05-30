1d6c7f151500c0e6013f429450c561ab543ebe85
John Huynh <j.n.huynh@me.com>
2013-05-30 11:32:49 -0400

    Removed unused aliases.

--- Bash/aliases.bash
+++ Bash/aliases.bash
@@ -6,11 +6,9 @@
 ####################################
 
 ##### SHELL ####################################################################
-alias blog="cd ~/Documents/Blog/DIRhuynh"
+
 alias c="clear"
-alias courses="cd ~/Documents/Courses"
 alias dirs="dirs -v"
-alias tstp="date +'%m%d%y_%H%M%S'"
 alias egrep="env | grep"
 alias grep="grep --color=always"
 alias hgrep="history | grep"
@@ -20,7 +18,6 @@ alias ll="ls -l -FGh"
 alias la="ls -la -FGh"
 alias reload="source ~/.bash_profile"
 alias sftpGator="sftp -P 2222 jnhuynh@74.53.180.5"
-alias sp="set_rails_project"
 alias ssh="ssh " # X11 Forwarding "ssh -X"
 alias sshC="ssh -N -g -D 8778 cs.uml.edu"
 alias sshGator="ssh -p 2222 jnhuynh@74.53.180.5"
@@ -28,29 +25,17 @@ alias v="vim"
 #alias v="mvim"
 
 ##### Git ######################################################################
+
 # alias g="git" # Disable to promote use of $git autocompletion
 alias cbr="git rev-parse --abbrev-ref HEAD"
 alias glgs="glgs.sh"
 alias gsts="gsts.sh"
 
 ##### Rail, Ruby, Bundle, RVM ##################################################
+
 alias b="bundle"
 alias be="bundle exec"
 alias bi="bundle install"
 alias bu="bundle update"
-alias papp="cd $PROJECT/app" # $PROJECT is defined in the environment file
-alias pctrl="cd $PROJECT/app/controllers"
-alias pmodl="cd $PROJECT/app/models"
-alias ptest="cd $PROJECT/test"
-alias proot="cd $PROJECT"
-alias pspec="cd $PROJECT/spec"
-alias psite="cd $PROJECT/sites"
-alias pview="cd $PROJECT/app/views"
-
-##### SOURCE CODE SEARCH #######################################################
-alias cfind="find . -type f -name "*.c""
-alias hfind="find . -type f -name "*.h""
-alias sfind="find . -type f -name "*.c" -o -name "*.h""
-alias rfind="find . -type f -name "*.rb" -o -name "*.erb""
 
 ################################################################################
