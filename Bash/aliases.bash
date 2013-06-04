84d2f68b7b4024d18a5786ede97ac44c0c02ff6b
John Huynh <j.n.huynh@me.com>
2013-06-04 19:58:15 -0400

    Added today alias for simplified timestamp.

--- Bash/aliases.bash
+++ Bash/aliases.bash
@@ -21,6 +21,7 @@ alias sftpGator="sftp -P 2222 jnhuynh@74.53.180.5"
 alias ssh="ssh " # X11 Forwarding "ssh -X"
 alias sshC="ssh -N -g -D 8778 cs.uml.edu"
 alias sshGator="ssh -p 2222 jnhuynh@74.53.180.5"
+alias today="date '+%Y_%m_%d'"
 alias v="vim"
 #alias v="mvim"
 
