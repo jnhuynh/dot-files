e7114a6515a98aeb21f01b2f3594aedd34a218c1
John Huynh <jn@huynh.io>
2013-12-13 00:58:59 -0500

    Added initial implementation of numbify.

--- bash/scripts/numbify.sh
+++ bash/scripts/numbify.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+# Numbify functions should be used as part of aliases. It's purpose is to
+# expose a numbered list of values that can be used for other commands. For
+# example, git log's hash values can be used git cherry-pick or git rebase or
+# git status for git add.
+# lg   = "!function f { source ${HOME}/.bash_profile; git log --pretty=format:\"%Cred%h%Creset - %an, %ar : %Cgreen%s\" --abbrev-commit | awk '{ print $1 }' | numbify; }; f; git status -sb"
+# st   = "!function f { source ${HOME}/.bash_profile; git status -sb | awk '{ print $2 }' | numbify; }; f; git status -sb"
+# sh   = show --word-diff=color
+
 # Expects a pipe of lines containing one word. Creates a numbered list that can
 # be used for quick text expansion. Each call to numbify will wipe out the
 # prior list.
 #
 # Example:
 #
-# $ git status -st | awk '{ print $2 }'
+# $ git status -st | awk '{ print $2 }' | numbify
 # => 1 master
 #    2 some_file
 #    3 another_file
 #      ...
 #    n nth_file
-numbify () {
+nfy () {
   tmp_file="${HOME}/.numbify.tmp"
-  counter=1;
+  counter=1; # Start at one to sed's line counter.
 
   # Remove old temp file.
   rm -f $tmp_file
   # Create a new empty one.
-  echo -n "" > $tmp_file
+  > $tmp_file
 
   while read line;
     do if [ -n "$line" ] ; then
@@ -37,13 +45,13 @@ numbify () {
 #
 # Example:
 #
-# $ git status -st | awk '{ print $2 }'
+# $ git add $(select_numbify 3)
 # => 1 master
 #    2 some_file
 #    3 another_file
 #      ...
 #    n nth_file
-select_numbify () {
+snfy () {
   if [ -n "$1" ] ; then
     tmp_file="${HOME}/.numbify.tmp";
     cat $tmp_file | sed -n "$1p" | awk '{ print $2 }';
