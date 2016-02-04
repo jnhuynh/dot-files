4d401fad3d3e9e5fb5ec72ee03fdf907f2f3056f
John Huynh <john@huynh.io>
2016-02-04 11:42:59 -0500

    Add default tmux pane splitting function

--- oh-my-zsh-custom/alias.zsh
+++ oh-my-zsh-custom/alias.zsh
@@ -11,3 +11,12 @@ alias trd="tmux resize-pane -D"
 alias trl="tmux resize-pane -L"
 alias trr="tmux resize-pane -R"
 alias tmw="tmux movew"
+
+split-default() {
+  tmux split-window -v
+  tmux swap-pane -D
+  tmux resize-pane -D 15
+  tmux split-window -h
+  tmux swap-pane -D
+  tmux send-keys 'C-l'
+}
